77deabae0535fbb1e1e3e3b1587e71643b483b5b
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-06-14 19:56:32 +0200

    1

--- src/models/model-helpers/MyBaseModel.js
+++ src/models/model-helpers/MyBaseModel.js
@@ -2,23 +2,27 @@ import { DBBaseModel } from 'quicklists-vue-orm-ui-2'
 import VueCookies from "vue-cookies";
 
 export default class DBEvent extends DBBaseModel {
-    // static baseUrl = 'http://2024-05-era.test'
-    static baseUrl = 'https://briland.co.za'
-    // static Apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MjEwNDAwLAogICJleHAiOiAxODUyMDYzMjAwCn0.1OA6JmidSrPG7-VKVIC51DIO0Jj2OxbtHknWYaAu4mw'
-    static adapator = 'laravel'
+  static baseUrl = 'https://briland.co.za'
+  // static Apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MjEwNDAwLAogICJleHAiOiAxODUyMDYzMjAwCn0.1OA6JmidSrPG7-VKVIC51DIO0Jj2OxbtHknWYaAu4mw'
+  static adapator = 'laravel'
 
-    static mergeHeaders(headers) {
-      const result = {
-        // Apikey: this.Apikey,
-        ...headers,
-      }
+  static mergeHeaders(headers) {
+    const result = {
+      ...headers,
+    };
 
-      const VITE_AUTH = VueCookies.get('VITE_AUTH')
-      if (VITE_AUTH?.token){
-        result['Authorization'] = `Bearer ${VITE_AUTH.token}`
+    const VITE_AUTH = VueCookies.get('VITE_AUTH');
+    if (VITE_AUTH?.token) {
+      const expireDate = new Date(VITE_AUTH.expireDate);
+      const currentDate = new Date();
+
+      if (currentDate <= expireDate) {
+        result['Authorization'] = `Bearer ${VITE_AUTH.accessToken}`;
       }
-      return result
     }
+
+    return result;
+  }
 
 
 
